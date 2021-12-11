76d569a5d593a7421489d037713ad5cb875c9bbb
Dmitry Kuzmenchuk <dmitry.kuzmenchuk@gmail.com>
2021-12-11 13:44:19 +0400

    feat(main): added more events

--- app/src/main/java/ru/dmitry/kuzmenchuk/newstackapp/ui/main/MainEvent.kt
+++ app/src/main/java/ru/dmitry/kuzmenchuk/newstackapp/ui/main/MainEvent.kt
@@ -5,15 +5,25 @@ import ru.dmitry.kuzmenchuk.newstackapp.domain.util.loadable.Loadable
 
 sealed class MainEvent {
 
-    data class SearchQueryChanged(val newSearchQuery: String) : MainEvent()
+    data class PhotoClicked(val photo: Photo): MainEvent()
 
     data class TabChanged(val tab: MainTab) : MainEvent()
 
-    data class PhotoClicked(val photo: Photo): MainEvent()
+    data class SearchQueryChanged(val newSearchQuery: String) : MainEvent()
+
+
 
     data class SearchPhotosRequest(val loadable: Loadable<Collection<Photo>>) : MainEvent()
 
     data class EditorialPhotosRequest(val loadable: Loadable<Collection<Photo>>) : MainEvent()
 
     data class RandomPhotoRequest(val loadable: Loadable<Photo>) : MainEvent()
+
+
+
+    object SearchPhotosRequested : MainEvent()
+
+    object EditorialPhotosRequested : MainEvent()
+
+    object RandomPhotoRequested : MainEvent()
 }
